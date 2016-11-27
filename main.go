371feda4d0ea06d0bdc348cfbd89489ccc129ba1
cmechenie <cristian.mechenie@gmail.com>
2016-11-27 12:10:58 +0200

    Initial commit

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
+)
+
+//templ represents a single template
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+//ServeHttp handles the http request
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, r)
+}
+func main() {
+	var addr = flag.String("addr", ":8080", "The address of the application.")
+	flag.Parse() //parse the flags
+	r := newRoom()
+	// root
+	http.Handle("/", &templateHandler{filename: "chat.html"})
+	http.Handle("/room", r)
+	//get the room going
+	go r.run()
+	//start the web server
+	log.Println("Startig the web server on ", *addr)
+	if err := http.ListenAndServe(*addr, nil); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
